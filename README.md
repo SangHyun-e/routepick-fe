12f390e2371c1b69c6c2e024b27119e98ef73a1d
SangHyun-e <66759329+SangHyun-e@users.noreply.github.com>
2025-09-17 23:19:28 +0900

    chore: add base .gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+# Route Pick FE (Flutter Web)
+- Monorepo 아님, FE 단독 리포
+- BE: Route_Pick_BE (Spring Boot)
---
+++ android/app/src/main/kotlin/io/routepick/route_pick_fe/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.routepick.route_pick_fe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
